edc5357d0283c995b327f137141f6dd6513cd216
ThangEthan <thangvu201098@gmail.com>
2021-07-23 23:20:38 +0300

    add comment

--- App.js
+++ App.js
@@ -14,16 +14,17 @@ app.get('/', function(req, res) {
             console.error(err)
             return
         }
+        //Regex with capture group
         let chapter_index_regex = /\n(\d{1})\.\ .+/g        //capture group: 1:number
         let chapter_regex = /\n((\d{1})\d{2})\.\ .+/g       //capture group: 1:number, 2:chapter index number
         let rule_index_regex = /\n((\d{3})\.\d+)\..+/g      //capture group: 1:number, 2:chapter number
         let rule_regex = /\n(\d{3}\.\d+)+[a-z].+/g           //capture group: 1:rule index number`
-    
+        //Extract numbered rule 
         let chapter_index = [...data.matchAll(chapter_index_regex)].map(x => { return { "number": x[1], "name": x[0].trim() } });
         let chapter = [...data.matchAll(chapter_regex)].map(x => { return { "number": x[1], "chapter_index_number": x[2], "name": x[0].trim() } })
         let rule_index = [...data.matchAll(rule_index_regex)].map(x => { return { "number": x[1], "chapter_number": x[2], "name": x[0].trim() } })
         let rule = [...data.matchAll(rule_regex)].map(x => { return { "rule_index_number": x[1], "name": x[0].trim() } })
-
+        //Join data
         let joinedRuleIndex = rule_index.map(x => {
             return rule.reduce(function (grouped, y) {
                 if (grouped.number == y.rule_index_number){
