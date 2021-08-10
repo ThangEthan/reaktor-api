93444a985dbaef728d7756cfbc5bfebec0e05938
ThangEthan <thangvu201098@gmail.com>
2021-08-10 20:22:45 +0300

    add more data to rule object

--- App.js
+++ App.js
@@ -18,17 +18,17 @@ app.get('/', function(req, res) {
         let chapter_index_regex = /\n(\d{1})\.\ .+/g        //capture group: 1:number
         let chapter_regex = /\n((\d{1})\d{2})\.\ .+/g       //capture group: 1:number, 2:chapter index number
         let rule_index_regex = /\n((\d{3})\.\d+)\..+/g      //capture group: 1:number, 2:chapter number
-        let rule_regex = /\n(\d{3}\.\d+)+[a-z].+/g           //capture group: 1:rule index number`
+        let rule_regex = /\n((\d{3}\.\d+)+[a-z]).+/g        //capture group: 1:rule index number`, 2:rule number
         //Extract numbered rule 
         let chapter_index = [...data.matchAll(chapter_index_regex)].map(x => { return { "number": x[1], "name": x[0].trim() } });
         let chapter = [...data.matchAll(chapter_regex)].map(x => { return { "number": x[1], "chapter_index_number": x[2], "name": x[0].trim() } })
         let rule_index = [...data.matchAll(rule_index_regex)].map(x => { return { "number": x[1], "chapter_number": x[2], "name": x[0].trim() } })
-        let rule = [...data.matchAll(rule_regex)].map(x => { return { "rule_index_number": x[1], "name": x[0].trim() } })
+        let rule = [...data.matchAll(rule_regex)].map(x => { return { "number": x[1], "rule_index_number": x[2], "name": x[0].trim() } })
         //Join data
         let joinedRuleIndex = rule_index.map(x => {
             return rule.reduce(function (grouped, y) {
                 if (grouped.number == y.rule_index_number){
-                    (grouped["rules"] = grouped["rules"] || []).push(y.name)
+                    (grouped["rules"] = grouped["rules"] || []).push(y)
                     return grouped;
                 }
                 else
@@ -58,11 +58,11 @@ app.get('/', function(req, res) {
             }, x)
         })
         res.json({
-            "joinedChapterIndex": joinedChapterIndex,
+            "joinedChapterIndex": joinedRuleIndex,
             "joinedChapter": joinedChapter
         })
     })
 });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server started at port ' + port);
